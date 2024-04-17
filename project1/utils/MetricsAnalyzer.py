4ec1e816afe2df8b901e6a8a8254b48523dda371
kristileebricker <128258703+kristileebricker@users.noreply.github.com>
2024-04-17 16:21:19 -0700

    Update MetricsAnalyzer.py
    Added a title and adjusted colors and labels for the ROC plot

--- project1/utils/MetricsAnalyzer.py
+++ project1/utils/MetricsAnalyzer.py
@@ -48,21 +48,20 @@ class MetricsAnalyzer ():
     def plot_ROC (self, title = None):
         prob_scores = self.model.predict_proba(self.data)
         fpr, tpr, _ = roc_curve(self.true_labels, prob_scores[:, 1], pos_label=self.pos_label)
-        roc_auc = auc(fpr, tpr)
+        roc_auc = auc(fpr, tpr) # Get the area under the curve
 
         if title:
             plt.title(title)
 
         # Plot the ROC curve
-        #plt.plot(fpr, tpr)
-        plt.plot(fpr, tpr, color='orange', label='ROC curve (area = %0.2f)' % roc_auc)
-        plt.plot([0, 1], [0, 1], 'k--', color='blue', label='No Skill')
+        plt.suptitle('ROC Curve')
+        plt.plot(fpr, tpr, color='blue', label='Trained Classifier' (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], 'k--', color='red', label='Random Classifier')
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
         plt.xlabel('False Positive Rate')
         plt.ylabel('True Positive Rate')
         plt.legend()
-        # show the plot
         plt.show()
     
     def print_confusion_matrix (self):
