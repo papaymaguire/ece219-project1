484e1ace119eba076e75c2d8a33783aee54656cc
kristileebricker <128258703+kristileebricker@users.noreply.github.com>
2024-04-17 16:07:49 -0700

    Update MetricsAnalyzer.py
    Changed the color of the ROC plots

--- project1/utils/MetricsAnalyzer.py
+++ project1/utils/MetricsAnalyzer.py
@@ -55,8 +55,8 @@ class MetricsAnalyzer ():
 
         # Plot the ROC curve
         #plt.plot(fpr, tpr)
-        plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
-        plt.plot([0, 1], [0, 1], 'k--', label='No Skill')
+        plt.plot(fpr, tpr, color='orange', label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], 'k--', color='blue', label='No Skill')
         plt.xlim([0.0, 1.0])
         plt.ylim([0.0, 1.05])
         plt.xlabel('False Positive Rate')
